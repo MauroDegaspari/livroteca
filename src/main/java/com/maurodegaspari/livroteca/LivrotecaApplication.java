4a292f96fd9d33edbaf33d2f4225307c722fd447
Mauro Degaspari <mauro_degaspari@hotmail.com>
2025-05-28 19:11:25 -0300

    projeto inicial

---
+++ src/main/java/com/maurodegaspari/livroteca/LivrotecaApplication.java
@@ -0,0 +1,13 @@
+package com.maurodegaspari.livroteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maurodegaspari/livroteca/LivrotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurodegaspari.livroteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
