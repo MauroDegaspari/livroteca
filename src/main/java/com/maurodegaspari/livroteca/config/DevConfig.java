1b0749037e16b1c6f7d68c59354755f8038acc7d
Mauro Degaspari <mauro_degaspari@hotmail.com>
2025-06-01 13:35:26 -0300

    Criação de acesso a banco dev e tst

---
+++ src/main/java/com/maurodegaspari/livroteca/config/DevConfig.java
@@ -0,0 +1,22 @@
+package com.maurodegaspari.livroteca.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.maurodegaspari.livroteca.services.DBtesteServices;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	DBtesteServices dev;
+	
+	public void chamaInsertBanco() {
+		
+		 this.dev.insertBancoTest();
+		
+	}
+
+}
---
+++ src/main/java/com/maurodegaspari/livroteca/config/TstConfig.java
@@ -0,0 +1,23 @@
+package com.maurodegaspari.livroteca.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.maurodegaspari.livroteca.services.DBtesteServices;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+@Profile("tst")
+public class TstConfig {
+
+	@Autowired
+	private DBtesteServices tst;
+
+	@PostConstruct // indica que um método deve ser executado automaticamente após o Spring instanciar o bean e injetar as dependências.
+	public void chamaBancoTst() {
+
+		tst.insertBancoTest();
+	}
+}
