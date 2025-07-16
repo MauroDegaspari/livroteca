dc5b1ad9ffa546430e8af8ce1e244a9c31a43001
Mauro Degaspari <mauro_degaspari@hotmail.com>
2025-07-16 08:13:42 -0300

    Front-end - frontEnd inicial

---
+++ livroteca-Frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl:'app.Component.html'
+})
+export class AppComponent {
+  title = 'livroteca-Frontend';
+}
